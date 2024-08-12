66746714238ef639c528b69608ca9186a7f3f5b6
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-12 13:56:16 +0700

    feat: add nextjs pages router

---
+++ apps/nextjs-pages/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { type AppType } from "next/app";
+import { api } from "@/utils/api";
+
+import "@/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default api.withTRPC(MyApp);
