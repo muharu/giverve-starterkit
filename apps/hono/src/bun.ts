db2b2d08bc4ba4135fa60e82c319f019e5e0bd7d
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-13 09:27:34 +0700

    fix: hono cors

--- apps/hono/src/bun.ts
+++ apps/hono/src/bun.ts
@@ -1,6 +1,16 @@
 import { PORT } from "./port";
 import app from "./root";
 
+/**
+ * Server setup for Hono Bun runtime
+ *
+ * Uses Bun’s native `Bun.serve` method to handle HTTP requests.
+ * - `fetch`: A reference to the Hono application’s fetch handler.
+ * - `port`: The port number on which the server will listen for incoming requests.
+ *
+ * Logs the port number to the console once the server is running.
+ * If Bun is not available, a message indicating that Bun is not installed is logged.
+ */
 if (typeof Bun !== "undefined" && process.versions.bun) {
   Bun.serve({
     fetch: app.fetch,
@@ -9,5 +19,7 @@ if (typeof Bun !== "undefined" && process.versions.bun) {
 
   console.log(`Server running on Bun Runtime at port ${PORT}`);
 } else {
-  console.log("Bun is not enabled");
+  console.log(
+    "Bun runtime is not detected. Please ensure that Bun is installed on your system.",
+  );
 }
--- apps/hono/src/root.ts
+++ apps/hono/src/root.ts
@@ -1,10 +1,34 @@
 import { trpcServer } from "@hono/trpc-server";
-import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { Hono } from "hono/tiny";
 
 import { appRouter } from "@giverve/api";
 
+// Create a new Hono application instance
 const app = new Hono();
 
+/**
+ * Middleware to handle CORS (Cross-Origin Resource Sharing).
+ *
+ * This middleware is used to enable CORS for all routes matching "/trpc/*".
+ * By default, it allows requests from all origins. For more specific CORS configurations,
+ * you can customize the options as needed.
+ *
+ * @see https://hono.dev/docs/middleware/builtin/cors
+ */
+app.use("/api/trpc/*", cors());
+
+/**
+ * Middleware to handle tRPC API requests.
+ *
+ * This middleware sets up the tRPC server with the provided router and endpoint.
+ * Requests to routes matching "/trpc/*" will be handled by the tRPC server.
+ *
+ * - `router`: Specifies the tRPC router instance to handle requests.
+ * - `endpoint`: Defines the base endpoint for tRPC requests.
+ *
+ * @see https://www.npmjs.com/package/@hono/trpc-server
+ */
 app.use(
   "/api/trpc/*",
   trpcServer({
