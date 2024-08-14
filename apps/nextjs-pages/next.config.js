7773bb172908406f4a78c84a468a5c67f66afdf9
Muhammad Harun Alrasyid <62083792+muharu@users.noreply.github.com>
2024-08-14 16:00:46 +0700

    feat: add docker integration (#3)
    * feat: add docker for nextjs-pages
    
    * feat: add docker for hono

--- apps/nextjs-pages/next.config.js
+++ apps/nextjs-pages/next.config.js
@@ -7,6 +7,7 @@ createJiti(fileURLToPath(import.meta.url))("./src/env");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: "standalone",
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
