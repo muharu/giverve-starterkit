226cf56a40ea2376319ff07837e4acff1319be3f
Muhammad Harun Alrasyid <muharu@proton.me>
2024-08-16 04:29:57 +0700

    feat: add trpc client to bot

--- apps/bot/src/events/ready/ready.ts
+++ apps/bot/src/events/ready/ready.ts
@@ -1,17 +1,28 @@
 import type { ExtendedClient } from "../../shard";
+import { trpc } from "../../trpc";
 
 export default async (client: ExtendedClient) => {
   console.log(`${client.user?.tag} is online!`);
 
-  if (client.cluster) {
-    try {
-      const guildCounts = await client.cluster.broadcastEval(
-        (c) => c.guilds.cache.size,
-      );
-      const totalGuilds = guildCounts.reduce((sum, count) => sum + count, 0);
+  try {
+    const { greeting } = await trpc.post.hello.query({ text: "Udin" });
+    console.log(greeting);
+
+    if (client.cluster) {
+      const totalGuilds = await getTotalGuildsFromShards(client);
       console.log(`${totalGuilds} total guilds`);
-    } catch (error) {
-      console.error("Error fetching guild count:", error);
     }
+  } catch (error) {
+    console.error(error);
   }
 };
+
+async function getTotalGuildsFromShards(client: ExtendedClient) {
+  if (client.cluster) {
+    const guildCounts = await client.cluster.broadcastEval(
+      (c) => c.guilds.cache.size,
+    );
+    const totalGuilds = guildCounts.reduce((sum, count) => sum + count, 0);
+    return totalGuilds;
+  }
+}
--- apps/nextjs-pages/src/utils/api.ts
+++ apps/nextjs-pages/src/utils/api.ts
@@ -16,7 +16,7 @@ import type { AppRouter } from "@giverve/api";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.NEXTJS_API_PORT ?? 3000}`; // dev SSR should use localhost
+  return process.env.API_BASE_URL; // dev SSR should use API_BASE_URL
 };
 
 /** A set of type-safe react-query hooks for your tRPC API. */
